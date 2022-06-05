6cf51e112bf283f89c90fb0069967fc619fc04df
Hycent0999 <hycent.ochonogor@gmail.com>
2022-06-05 16:05:30 -0700

    put c

---
+++ 4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * File name;rubbish
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle.
+ * Return: is 0.
+ */
+int main(0);
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
